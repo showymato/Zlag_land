7292841559edfc3f1d8d7cce7bf7923c31e33b14
Karan <kh491198@gmail.com>
2025-08-13 12:52:20 +0530

    infos update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -28,8 +28,8 @@ const Pricing = () => {
         </div>
 
         <Heading
-          tag="Get started with Brainwave"
-          title="Pay once, use forever"
+          tag="Get started with Zerolag"
+          title="Pay once, Deploy your agent forever"
         />
 
         <div className="relative">
