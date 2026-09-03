00683e31965292158116b2f6b78f819fe2540583
Karan <kh491198@gmail.com>
2026-09-03 12:04:40 +0530

    HEADER logo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,129 +1,12 @@
-// import { useLocation } from "react-router-dom";
-// import { disablePageScroll, enablePageScroll } from "scroll-lock";
-
-// import { brainwave } from "../assets";
-// import { navigation } from "../constants";
-// import Button from "./Button";
-// import MenuSvg from "../assets/svg/MenuSvg";
-// import { HamburgerMenu } from "./design/Header";
-// import { useState } from "react";
-
-// const Header = () => {
-//   const pathname = useLocation();
-//   const [openNavigation, setOpenNavigation] = useState(false);
-
-//   const toggleNavigation = () => {
-//     if (openNavigation) {
-//       setOpenNavigation(false);
-//       enablePageScroll();
-//     } else {
-//       setOpenNavigation(true);
-//       disablePageScroll();
-//     }
-//   };
-
-//   const handleClick = () => {
-//     if (!openNavigation) return;
-
-//     enablePageScroll();
-//     setOpenNavigation(false);
-//   };
-
-//   return (
-//     <div
-//       className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
-//         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-//       }`}
-//     >
-//       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-//         <a className="block w-[12rem] xl:mr-8" href="#hero">
-//           <img src={brainwave} width={190} height={40} alt="Brainwave" />
-//         </a>
-
-//         <nav
-//           className={`${
-//             openNavigation ? "flex" : "hidden"
-//           } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
-//         >
-//           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-//             {navigation.map((item) => (
-//               <a
-//                 key={item.id}
-//                 href={item.url}
-//                 onClick={handleClick}
-//                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-//                   item.onlyMobile ? "lg:hidden" : ""
-//                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-//                   item.url === pathname.hash
-//                     ? "z-2 lg:text-n-1"
-//                     : "lg:text-n-1/50"
-//                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-//               >
-//                 {item.title}
-//               </a>
-//             ))}
-
-//              <a
-//                 key={6}
-//                 href="https://zerolag.gitbook.io/zerolag"
-//                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-//                   item.onlyMobile ? "lg:hidden" : ""
-//                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-//                   item.url === pathname.hash
-//                     ? "z-2 lg:text-n-1"
-//                     : "lg:text-n-1/50"
-//                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-//               >
-//                 whitepaper
-//               </a>
-
-//           </div>
-
-//           <HamburgerMenu />
-//         </nav>
-
-   
-//         <Button className="hidden lg:flex" href="#login">
-//           Connect Wallet
-//         </Button>
-
-//         <Button
-//           className="ml-auto lg:hidden"
-//           px="px-3"
-//           onClick={toggleNavigation}
-//         >
-//           <MenuSvg openNavigation={openNavigation} />
-//         </Button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { useState } from "react";
 
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
+
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
-import { HamburgerMenu } from "./design/Header";
-import { useState } from "react";
 
 const Header = () => {
   const { hash } = useLocation();
@@ -139,77 +22,287 @@ const Header = () => {
     }
   };
 
-  const handleClick = () => {
-    if (!openNavigation) return;
-    enablePageScroll();
+  const closeNavigation = () => {
     setOpenNavigation(false);
+    enablePageScroll();
   };
 
   return (
-    <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
-        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-      }`}
-    >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        {/* Logo */}
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+    <header className="fixed top-0 left-0 z-[100] w-full px-4 pt-4 lg:px-8">
+
+      {/* HEADER */}
+      <div
+        className="
+          relative
+          mx-auto
+          flex
+          h-[72px]
+          max-w-[1450px]
+          items-center
+
+          rounded-[18px]
+
+          border
+          border-white/10
+
+          bg-[#0b0912]/80
+          px-5
+
+          shadow-[0_8px_40px_rgba(0,0,0,0.35)]
+
+          backdrop-blur-xl
+
+          lg:px-7
+        "
+      >
+
+        {/* LOGO */}
+        <a
+          href="#hero"
+          className="
+            flex
+            h-[150px]
+            w-[170px]
+            shrink-0
+            items-center
+          "
+        >
+          <img
+            src={brainwave}
+            alt="ZeroLag"
+            className="h-full w-full object-contain"
+          />
         </a>
 
-        {/* Navigation */}
+        {/* DESKTOP NAV */}
         <nav
-          className={`${
-            openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
-        >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            {navigation.map((item) => (
-              <a
-                key={item.id}
-                href={item.url}
-                onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === hash ? "z-2 lg:text-n-1" : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-              >
-                {item.title}
-              </a>
-            ))}
+          className="
+            absolute
+            left-1/2
+            hidden
+            -translate-x-1/2
 
-            {/* Extra Whitepaper Link */}
+            lg:flex
+            lg:h-full
+            lg:items-center
+          "
+        >
+          <div className="flex items-center gap-8 xl:gap-11">
+
+            {navigation.map((item) => {
+              if (item.onlyMobile) return null;
+
+              const active = item.url === hash;
+
+              return (
+                <a
+                  key={item.id}
+                  href={item.url}
+                  className={`
+                    relative
+                    flex
+                    h-full
+                    items-center
+
+                    whitespace-nowrap
+
+                    font-code
+                    text-[12px]
+                    font-semibold
+                    uppercase
+                    tracking-wide
+
+                    transition-all
+                    duration-200
+
+                    ${
+                      active
+                        ? "text-white"
+                        : "text-white/45"
+                    }
+
+                    hover:text-white
+                  `}
+                >
+                  {item.title}
+
+                  {active && (
+                    <span
+                      className="
+                        absolute
+                        bottom-[14px]
+                        left-1/2
+                        h-[2px]
+                        w-5
+                        -translate-x-1/2
+                        rounded-full
+                        bg-gradient-to-r
+                        from-cyan-300
+                        via-purple-400
+                        to-pink-400
+                      "
+                    />
+                  )}
+                </a>
+              );
+            })}
+
+            {/* WHITEPAPER */}
             <a
-              key="whitepaper"
               href="https://zerolag.gitbook.io/zerolag"
-              onClick={handleClick}
-              className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                hash === "#whitepaper" ? "z-2 lg:text-n-1" : "lg:text-n-1/50"
-              } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="
+                flex
+                h-full
+                items-center
+
+                whitespace-nowrap
+
+                font-code
+                text-[12px]
+                font-semibold
+                uppercase
+                tracking-wide
+
+                text-white/45
+                transition-colors
+
+                hover:text-white
+              "
             >
               Whitepaper
             </a>
-          </div>
 
-          <HamburgerMenu />
+          </div>
         </nav>
 
-        {/* Wallet Button */}
-        <Button className="hidden lg:flex" href="#login">
-          Connect Wallet
-        </Button>
+        {/* WALLET */}
+        <div className="ml-auto hidden lg:block">
+          <Button
+            href="#login"
+            className="
+              !h-[46px]
+              !min-w-[165px]
+              !px-6
 
-        {/* Mobile Menu Toggle */}
-        <Button
-          className="ml-auto lg:hidden"
-          px="px-3"
+              text-[12px]
+            "
+          >
+            Connect Wallet
+          </Button>
+        </div>
+
+        {/* MOBILE */}
+        <button
           onClick={toggleNavigation}
+          className="
+            ml-auto
+            flex
+            h-[44px]
+            w-[44px]
+            items-center
+            justify-center
+
+            rounded-xl
+
+            border
+            border-white/10
+
+            bg-white/5
+
+            lg:hidden
+          "
         >
           <MenuSvg openNavigation={openNavigation} />
-        </Button>
+        </button>
       </div>
-    </div>
+
+      {/* MOBILE NAV */}
+      {openNavigation && (
+        <div
+          className="
+            mx-4
+            mt-2
+            rounded-[18px]
+
+            border
+            border-white/10
+
+            bg-[#0b0912]/95
+
+            p-5
+
+            backdrop-blur-xl
+
+            lg:hidden
+          "
+        >
+          <div className="flex flex-col">
+
+            {navigation.map((item) => {
+              if (item.onlyMobile) return null;
+
+              return (
+                <a
+                  key={item.id}
+                  href={item.url}
+                  onClick={closeNavigation}
+                  className="
+                    border-b
+                    border-white/5
+                    py-4
+
+                    font-code
+                    text-sm
+                    font-semibold
+                    uppercase
+                    text-white/60
+
+                    last:border-0
+
+                    hover:text-white
+                  "
+                >
+                  {item.title}
+                </a>
+              );
+            })}
+
+            <a
+              href="https://zerolag.gitbook.io/zerolag"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="
+                border-b
+                border-white/5
+                py-4
+
+                font-code
+                text-sm
+                font-semibold
+                uppercase
+                text-white/60
+
+                hover:text-white
+              "
+            >
+              Whitepaper
+            </a>
+
+            <div className="pt-5">
+              <Button
+                href="#login"
+                className="w-full"
+              >
+                Connect Wallet
+              </Button>
+            </div>
+
+          </div>
+        </div>
+      )}
+    </header>
   );
 };
 
